e917956c3a9a1d9cdea748d59c108e3e7668340c
Linmiao Xu <linmiao.xu@gmail.com>
2015-10-26 23:55:15 -0700

    Minor cleanup

--- app/controllers/favicons_controller.rb
+++ app/controllers/favicons_controller.rb
@@ -3,21 +3,12 @@ class FaviconsController < ApplicationController
   # Homepage - shows a list of recent favicons
   #
   def index
-    favicon_urls = Favicon::Cache.get_cached_favicon_urls
-    hostnames = open(Rails.root.join("hostnames")).read.strip.split(/\n/)
-    unless favicon_urls.present?
-      favicon_urls = %w( yahoo.com quora.com google.com )
-    end
-    favicon_urls = (favicon_urls + hostnames).uniq
-    @favicon_urls = favicon_urls.map {|url|
-      "/favicons?q=#{url}"
-    }
-
     @use_spritesheet = true
-
     if @use_spritesheet
       @spritesheet = Spritesheet.new
       @spritesheet.generate
+    else
+      set_favicon_urls
     end
   end
 
@@ -32,6 +23,20 @@ class FaviconsController < ApplicationController
     else
       render :nothing => true, :status => 404
     end
+  end
+
+  private
+
+  def set_favicon_urls
+    favicon_urls = Favicon::Cache.get_cached_favicon_urls
+    hostnames = open(Rails.root.join("hostnames")).read.strip.split(/\n/)
+    unless favicon_urls.present?
+      favicon_urls = %w( yahoo.com quora.com google.com )
+    end
+    favicon_urls = (favicon_urls + hostnames).uniq
+    @favicon_urls = favicon_urls.map {|url|
+      "/favicons?q=#{url}"
+    }
   end
 
 end
