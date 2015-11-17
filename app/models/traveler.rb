c302e9afcfaea949f487918fc0ec49914a880787
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-16 18:43:00 -0800

    Move beanstalkd logic to another file

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,11 +1,12 @@
 class Traveler
 
-  # STATUSES = ["active", "inactive", "resting", "extended break"]
+  # STATUSES = ["active", "resting", "inactive"]
+
+  attr_reader :logger, :directions
 
   def initialize
     @logger = Traveler::Logger.new
-    @beanstalk = Beaneater.new('localhost:11300')
-    @tube = @beanstalk.tubes["favicon_urls"]
+    @directions = nil
   end
 
   def status
@@ -55,37 +56,11 @@ class Traveler
     @tube.put url, :pri => priority
   end
 
-  def run
-    @logger.log "Watching for urls in Beanstalkd queue"
-    set_status "active"
-    begin
-      while (job = @tube.reserve)
-        url = job.body
-        @logger.log "Checking: #{url}"
-        begin
-          snapshot = FaviconSnapshot.find_or_init_with_query(url)
-          snapshot.init_from_fetcher_results
-          snapshot.save!
-        rescue => error
-          @error_handler = Traveler::ErrorHandler.new(error)
-          @logger.log "Failed to fetch for #{url}"
-          @logger.error error, :log_backtrace => @error_handler.show_backtrace?
-          sleep(5) if @error_handler.should_delay?
-          unless @error_handler.should_ignore?
-            set_status "resting"
-            state = export_state({ :fetcher => snapshot.fetcher, :error => error })
-            write_state_as_fixture(state)
-            binding.pry
-            add_url url, 1
-          end
-        ensure
-          snapshot = nil
-        end
-        job.delete
-      end
-    ensure
-      set_status "inactive"
-      @beanstalk.close
+  def run(source = :beanstalkd)
+    if source == :beanstalkd
+      @directions = Directions::Beanstalkd.new(self)
+      @logger.log "Watching for urls in Beanstalkd queue"
+      @directions.follow
     end
   end
 
---
+++ app/models/traveler/directions/beanstalkd.rb
@@ -0,0 +1,50 @@
+class Traveler::Directions::Beanstalkd
+
+  def initialize(traveler)
+    @traveler = traveler
+    @logger = traveler.logger
+    @beanstalk = Beaneater.new('localhost:11300')
+    @tube = @beanstalk.tubes["favicon_urls"]
+  end
+
+  def add_url(url, priority = 10)
+    @tube.put url, :pri => priority
+  end
+
+  def follow
+    @traveler.set_status "active"
+    begin
+      while (job = @tube.reserve)
+        url = job.body
+        @logger.log "Checking: #{url}"
+        begin
+          snapshot = FaviconSnapshot.find_or_init_with_query(url)
+          snapshot.init_from_fetcher_results
+          snapshot.save!
+        rescue => error
+          @error_handler = Traveler::ErrorHandler.new(error)
+          @logger.log "Failed to fetch for #{url}"
+          @logger.error error, :log_backtrace => @error_handler.show_backtrace?
+          sleep(5) if @error_handler.should_delay?
+          unless @error_handler.should_ignore?
+            @traveler.set_status "resting"
+            state = @traveler.export_state({
+              :fetcher => snapshot.fetcher,
+              :error   => error
+            })
+            @traveler.write_state_as_fixture(state)
+            binding.pry
+            add_url url, 1
+          end
+        ensure
+          snapshot = nil
+        end
+        job.delete
+      end
+    ensure
+      @traveler.set_status "inactive"
+      @beanstalk.close
+    end
+  end
+
+end
