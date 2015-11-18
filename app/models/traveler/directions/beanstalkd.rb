b49733de8ee44fe4183a0f88b4a5a5b22f9a8eda
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-18 11:17:43 -0800

    Add ErrorEvader to get out of crappy neighborhoods more quickly

--- app/models/traveler/directions/beanstalkd.rb
+++ app/models/traveler/directions/beanstalkd.rb
@@ -1,10 +1,49 @@
 class Traveler::Directions::Beanstalkd
 
+  # Tracks # sequential errors to determine how many
+  # urls to be skipped
+  #
+  class ErrorEvader
+
+    attr_accessor :n_seq_errors
+
+    def initialize(tube)
+      @tube = tube
+      @n_seq_errors = 0
+      @i = nil
+    end
+
+    def track_index(i)
+      if !@i.nil? && if i - @i > 1
+        @n_seq_errors = 0
+      end
+      @n_seq_errors += 1
+      puts "i: #{i}, seq: #{@n_seq_errors}"
+      @i = i
+    end
+
+    def should_evade?
+      @n_seq_errors >= 10
+    end
+
+    def evade!
+      return unless should_evade?
+      @n_seq_errors.times do |i|
+        job = @tube.reserve
+        job.delete
+      end
+      @n_seq_errors
+    end
+
+  end
+
+
   def initialize(traveler)
     @traveler = traveler
     @logger = traveler.logger
     @beanstalk = Beaneater.new('localhost:11300')
     @tube = @beanstalk.tubes["favicon_urls"]
+    @evader = ErrorEvader.new(@tube)
   end
 
   def add_url(url, priority = 10)
@@ -14,39 +53,44 @@ class Traveler::Directions::Beanstalkd
   def follow
     @logger.log "Following urls in Beanstalkd tube - #{@tube.name}"
     @traveler.set_status "active"
-    begin
-      while (job = @tube.reserve)
-        url = job.body
-        @logger.log "Checking: #{url}"
-        begin
-          snapshot = FaviconSnapshot.find_or_init_with_query(url)
-          snapshot.init_from_fetcher_results
-          snapshot.save!
-        rescue => error
-          @error_handler = Traveler::ErrorHandler.new(error)
-          @logger.log "Failed to fetch for #{url}"
-          @logger.error error, :log_backtrace => @error_handler.show_backtrace?
-          sleep(5) if @error_handler.should_delay?
-          unless @error_handler.should_ignore?
-            @traveler.set_status "resting"
-            state = @traveler.export_state({
-              :fetcher => snapshot.fetcher,
-              :error   => error
-            })
-            @traveler.write_state_as_fixture(state)
-            binding.pry
-            add_url url, 1
-          end
-          @traveler.set_status "active"
-        ensure
-          snapshot = nil
+    i = 0
+    loop do
+      job = @tube.reserve
+      url = job.body
+      @logger.log "Checking: #{url}"
+      begin
+        snapshot = FaviconSnapshot.find_or_init_with_query(url)
+        snapshot.init_from_fetcher_results
+        snapshot.save!
+      rescue => error
+        error_handler = Traveler::ErrorHandler.new(error)
+        @logger.log "Failed to fetch for #{url}"
+        @logger.error error, :log_backtrace => error_handler.show_backtrace?
+        @evader.track_index i
+        if (n = @evader.evade!)
+          @logger.log "Too many sequential errors. Evading the next #{n} urls"
         end
-        job.delete
+        sleep(5) if error_handler.should_delay?
+        unless error_handler.should_ignore?
+          @traveler.set_status "resting"
+          state = @traveler.export_state({
+            :fetcher => snapshot.fetcher,
+            :error   => error
+          })
+          @traveler.write_state_as_fixture(state)
+          binding.pry
+          add_url url, 1
+        end
+        @traveler.set_status "active"
+      ensure
+        snapshot = nil
       end
-    ensure
-      @traveler.set_status "inactive"
-      @beanstalk.close
+      job.delete
+      i += 1
     end
+  ensure
+    @traveler.set_status "inactive"
+    @beanstalk.close
   end
 
 end
