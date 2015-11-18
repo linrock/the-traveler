5632f5251628d76f657b399fc4f0add0bb746ed7
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-17 17:51:19 -0800

    Colorize stdout log output

--- app/models/traveler/logger.rb
+++ app/models/traveler/logger.rb
@@ -5,17 +5,36 @@ class Traveler::Logger
     # @error_file = File.open(Rails.root.join("log/beanstalk.log"), "a+")
   end
 
-  def log(message)
-    formatted = "[#{Time.now}] #{message}"
-    puts formatted
-    @log_file.write formatted + "\n"
+  def log(message, options = {})
+    log_to_file(message)
+    log_to_stdout(message, options)
+  end
+
+  def log_to_file(message)
+    @log_file.write "[#{Time.now}] #{message}\n"
     @log_file.flush
   end
 
+  def log_to_stdout(message, options = {})
+    timestamp = colorize("#{Time.now}", "1;37")
+    if options[:color_code]
+      formatted = "[#{timestamp}] #{colorize(message, options[:color_code])}"
+    else
+      formatted = "[#{timestamp}] #{message}"
+    end
+    puts formatted
+  end
+
   def error(error, options = {})
-    message = "#{error.class}: #{error.message}\n"
+    message = "#{error.class}: #{error.message}"
     message = message + error.backtrace.join("\n") if options[:log_backtrace]
-    log message
+    log message, :color_code => "1;33"
+  end
+
+  private
+
+  def colorize(text, color_code)
+    "\e[#{color_code}m#{text}\e[0m"
   end
 
 end
