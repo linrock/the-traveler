e8e1bf0140693520910c6920767895f3fd9d7f63
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-18 20:41:18 -0800

    Add domain scout for finding unique domains to visit

---
+++ app/models/domain.rb
@@ -0,0 +1,31 @@
+class Domain < ActiveRecord::Base
+  validates_format_of :url, :with => /\A[\w\-]+(\.[\w\-]+)*(\.[a-z]{2,})\z/
+
+  after_initialize :normalize_url
+
+  def self.uncharted(n)
+    where(:visited => false).order('id ASC').limit(n)
+  end
+
+  def visit!
+    html_response = nil
+    begin
+      html_response = Favicon::HTTPClient.get(self.url)
+    rescue Favicon::CurlError => error
+      self.error_message = error.message
+    end
+    self.visited = true
+    self.successful = !self.error_message.present?
+    self.last_visit_at = Time.now
+    self.save!
+    html_response
+  end
+
+  def normalize_url
+    if self.url !~ /\Ahttps?:\/\//
+      self.url = "http://#{self.url}"
+    end
+    self.url = URI.parse(url).hostname.downcase
+  end
+
+end
---
+++ app/models/scout.rb
@@ -0,0 +1,83 @@
+# The scout explores unvisited domains and finds prospective
+# domains to visit in the future
+#
+class Scout
+
+  LOG_FILE        = "log/scout.log"
+  BEANSTALK_HOST  = "localhost:11300"
+  BEANSTALK_TUBE  = "domains_to_visit"
+  MAX_QUEUE_SIZE  = 20000
+
+  def initialize
+    @logger = ColorizedLogger.new(LOG_FILE)
+    @beanstalk = Beaneater.new(BEANSTALK_HOST)
+    @tube = @beanstalk.tubes[BEANSTALK_TUBE]
+  end
+
+  def process_domain(domain)
+    @logger.log "Visiting #{domain.url}"
+    html = domain.visit!
+    if domain.error_message
+      @logger.error "Failed: #{domain.error_message}"
+      return
+    end
+    enqueue_url domain.url
+    urls = find_unique_domains_in_html(html)
+    save_uncharted_domains urls
+  end
+
+  def save_uncharted_domains(urls)
+    i = 0
+    ActiveRecord::Base.transaction do
+      urls.each do |url|
+        domain = Domain.find_by(:url => url)
+        next if domain.present?
+        Domain.create!(:url => url)
+        i += 1
+      end
+    end
+    @logger.log "Found #{i} uncharted out of #{urls.length} domains"
+  end
+
+  def visit_uncharted_domains(n = 50)
+    Domain.uncharted(n).each { |domain| process_domain(domain) }
+  end
+
+  def find_unique_domains_from_url(url)
+    process_domain Domain.find_or_create_by(:url => url)
+  end
+
+  def find_unique_domains_in_html(html)
+    doc = Nokogiri::HTML.parse html
+    links = doc.css("a").map {|a| a.attr("href") }.
+                select {|href| (href =~ /\Ahttps?:\/\//) rescue nil }.
+                map    {|href| URI.parse(href).hostname.downcase rescue nil }.
+                compact.uniq
+  end
+
+  def enqueue_url(url, priority = 10)
+    @tube.put url, :pri => priority
+  end
+
+  def queue_size
+    @tube.stats[:current_jobs_ready]
+  end
+
+  def queue_is_fat?
+    queue_size > MAX_QUEUE_SIZE
+  end
+
+  def explore!
+    loop do
+      if queue_is_fat?
+        @logger.log "Taking a break - queue is pretty full (#{queue_size})"
+        sleep(60)
+        next
+      end
+      visit_uncharted_domains
+    end
+  ensure
+    @beanstalk.close
+  end
+
+end
