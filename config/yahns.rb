bbe523df42a9fe192a48b30bc67931389e1a97a6
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-20 00:03:16 -0800

    Update yahns. Move yahns config file

--- config/yahns.rb
+++ config/yahns.rb
@@ -12,26 +12,24 @@ worker_processes(1) do
   end
 
   atfork_parent do
-    puts "#$$ this is probably not useful"
+    # puts "#$$ this is probably not useful"
   end
 
 end
 
-# working_directory "/path/to/my_app"
-# stdout_path "log/yahns.out.log"
-stderr_path "log/yahns.err.log"
-pid "tmp/pids/yahns.pid"
-client_expire_threshold 0.5
 
-queue do
-  worker_threads 50
-end
+# working_directory "/path/to/my_app"
+
+# stdout_path               "log/yahns.out.log"
+stderr_path               "log/yahns.err.log"
+pid                       "tmp/pids/yahns.pid"
+client_expire_threshold    0.5
 
 app(:rack, "config.ru", preload: false) do
-  listen 3000
-  client_max_body_size 1024 * 1024
-  input_buffering true
-  output_buffering true # this lazy by default
-  client_timeout 5
-  persistent_connections true
+  listen                   3000
+  client_max_body_size     1024 * 1024
+  input_buffering          true
+  output_buffering         true # this lazy by default
+  client_timeout           5
+  persistent_connections   true
 end
