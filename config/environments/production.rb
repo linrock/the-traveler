cf641317edd2f08248529a9db35a3f0d2d45434e
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-20 02:46:32 -0800

    Set custom relative_url_root via ENV in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.relative_url_root = ENV["RELATIVE_URL_ROOT"]
 end
