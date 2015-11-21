7e5926f4a1a184d7d60de2b7501c940f8fe1225a
Linmiao Xu <linmiao.xu@gmail.com>
2015-11-21 11:40:09 -0800

    Replay last 10 most recently found favicons

--- app/controllers/traveler_controller.rb
+++ app/controllers/traveler_controller.rb
@@ -4,7 +4,7 @@ class TravelerController < ApplicationController
 
   def index
     @traveler = Traveler.new
-    @favicon_snapshots = FaviconSnapshot.offset(5).get_recent_favicons
+    @favicon_snapshots = FaviconSnapshot.offset(10).get_recent_favicons
     @ids = [ @favicon_snapshots.first ? @favicon_snapshots.first.id : 0 ,
              @favicon_snapshots.last  ? @favicon_snapshots.last.id  : 0 ]
     # @favicon_snapshots = FaviconSnapshot.order("id DESC").offset(30).limit(120)
