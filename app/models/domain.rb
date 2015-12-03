18ecdbbf1cd3a7c0fdd12bc8e2d0f943ae31d6f8
Linmiao Xu <linmiao.xu@gmail.com>
2015-12-02 20:13:48 -0800

    Delete url prefixing to new favicon party http client

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -12,8 +12,7 @@ class Domain < ActiveRecord::Base
   def visit!
     html_response = nil
     begin
-      prefixed_url = FaviconParty::Utils.prefix_url(self.url)
-      html_response = FaviconParty::HTTPClient.get(prefixed_url)
+      html_response = FaviconParty::HTTPClient.get(url)
       self.error_message = nil
     rescue FaviconParty::CurlError => error
       self.error_message = error.message
